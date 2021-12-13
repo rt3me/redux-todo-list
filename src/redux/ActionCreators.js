c8ed0f2bb4e0a8c7af66c543eae904cb37f291a7
Robert Turner <robturneris@gmail.com>
2021-12-12 23:03:47 -0500

    Updated action creators

--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,26 +1,35 @@
-import * as ActionTypes from './ActionTypes'
+import * as ActionTypes from "./ActionTypes";
 
 export const addTodo = (todo) => (dispatch) => {
+  console.log("addTodo called");
   const newTodo = {
     activity: todo,
     complete: false,
-  }
-  dispatch(createTask('What should go here?'))
-}
+  };
+  dispatch(createTask(newTodo));
+};
 
 const createTask = (todo) => ({
   // Task: Change this object to a redux action.
-})
+  type: ActionTypes.ADD_TODO,
+  payload: todo,
+});
 
 export const toggleToDo = (id) => ({
   type: ActionTypes.TOGGLE_COMPLETE,
   payload: id,
-})
+});
 
 export const clearAllTasks = () => ({
   type: ActionTypes.CLEAR_TASKS,
-})
+});
 
 export const deleteAllTasks = () => ({
   // Task: Create an object to dispatch. use an object in this file as an example
-})
+  type: ActionTypes.DELETE_TASKS,
+});
+
+export const deleteSingleTask = (id) => ({
+  type: ActionTypes.DELETE_SINGLE_TASK,
+  payload: id,
+});
