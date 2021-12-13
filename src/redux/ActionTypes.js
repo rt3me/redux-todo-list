eff9b1c10da6e57ec35b7dc6552b9a75e7c486bd
Robert Turner <robturneris@gmail.com>
2021-12-12 23:03:28 -0500

    Updated action types

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,5 +1,5 @@
-export const ADD_TODO = 'ADD_TODO'
-export const TOGGLE_COMPLETE = 'TOGGLE_COMPLETE'
-export const CLEAR_TASKS = 'CLEAR_TASKS'
-export const DELETE_TASKS = 'DELETE_TASKS'
-export const CHANGEME = "This doesn't do anything it's just a placeholder"
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_COMPLETE = "TOGGLE_COMPLETE";
+export const CLEAR_TASKS = "CLEAR_TASKS";
+export const DELETE_TASKS = "DELETE_TASKS";
+export const DELETE_SINGLE_TASK = "DELETE_SINGLE_TASK";
